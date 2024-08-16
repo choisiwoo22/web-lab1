05fe25465742fd97916c7a70ab9cd8e4d4921540
dddecevesverv <duynguyenst15@gmail.com>
2024-08-16 14:43:04 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./routes/contact.route.js");
 const app = express();
 
 app.use(cors());
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, resp) => {
   resp.json({ message: "xin chao ban den voi ung dung quan li danh ba!" });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
